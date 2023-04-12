0441cfa1071a9942a18e39c50d9005e8667f4019
daniar-17 <daniarnuramin1@gmail.com>
2023-04-12 09:51:23 +0700

    Multiple Row Push

--- app/Http/Controllers/CekOngkirController.php
+++ app/Http/Controllers/CekOngkirController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use GuzzleHttp\Client;
@@ -10,7 +11,20 @@ class CekOngkirController extends Controller
 {
     public function index()
     {
-        return view('ongkir.index');
+        $users = User::all();
+        $client = new Client();
+        $url = "https://api.rajaongkir.com/starter/city";
+        $params = [
+        ];
+        $headers = [
+            'key' => '695fd2e1535a3750238404ee731b0ebd'
+        ];
+        $response = $client->request('GET', $url, [
+            'headers' => $headers,
+            'verify'  => false,
+        ]);
+        $responseBody = json_decode($response->getBody());
+        return view('ongkir.index', compact('responseBody'));
     }
 
     public function cekongkir()
@@ -25,6 +39,13 @@ class CekOngkirController extends Controller
             "courier"=> "jne"
         ];
 
+        $params2 = [
+            "origin"=> "501",
+            "destination"=> "114",
+            "weight"=> 1700,
+            "courier"=> "pos"
+        ];
+
         $headers = [
             'key' => '695fd2e1535a3750238404ee731b0ebd'
         ];
@@ -35,7 +56,14 @@ class CekOngkirController extends Controller
             'verify'  => false,
         ]);
 
-        $responseBody = json_decode($response->getBody());
+        $response2 = $client->request('POST', $url, [
+            'form_params' => $params2,
+            'headers' => $headers,
+            'verify'  => false,
+        ]);
+
+        $responseBody[] = json_decode($response->getBody());
+        $responseBody[] = json_decode($response2->getBody());
         dd($responseBody);
     }
     //Last Line
--- resources/views/ongkir/index.blade.php
+++ resources/views/ongkir/index.blade.php
@@ -23,7 +23,6 @@
     <div class="card-header align-items-center d-flex">
       <h4>Cek Ongkir</h4>
     </div>
-    <!-- end card header -->
     <div class="card-body">
       <div class="row">
         <div class="col-xl-4">
@@ -31,15 +30,15 @@
                 <div class="card-body p-0">
                     <div class="alert alert-primary border-0 rounded-top rounded-0 m-0 d-flex align-items-center" role="alert">
                         <div class="flex-grow-1 text-truncate">
-                            <b>Asal Pengiriman</b>.
+                            <b>Asal Pengiriman</b>. 
                         </div>
                     </div>
                     <div class="d-flex p-2">
                         <select class="form-control js-example-basic-single asal" name="state">
                             <option value="">Asal Pengiriman</option>
-                            <option value="Choice 1">Choice 1</option>
-                            <option value="Choice 2">Choice 2</option>
-                            <option value="Choice 3">Choice 3</option>
+                            @foreach ( $responseBody->rajaongkir->results as $item )
+                              <option value="{{ $item->city_id }}">{{ $item->type }}, {{ $item->city_name }}, {{ $item->province }}</option>
+                            @endforeach
                         </select>
                     </div>
                 </div>
@@ -56,9 +55,9 @@
                     <div class="d-flex p-2">
                         <select class="form-control js-example-basic-single tujuan" name="state">
                             <option value="">Tujuan Pengiriman</option>
-                            <option value="Choice 1">Choice 1</option>
-                            <option value="Choice 2">Choice 2</option>
-                            <option value="Choice 3">Choice 3</option>
+                            @foreach ( $responseBody->rajaongkir->results as $item )
+                              <option value="{{ $item->city_id }}">{{ $item->type }}, {{ $item->city_name }}, {{ $item->province }}</option>
+                            @endforeach
                         </select>
                     </div>
                 </div>
@@ -73,7 +72,7 @@
                         </div>
                     </div>
                     <div class="d-flex p-2">
-                        <input type="text" class="form-control" id="berat" placeholder="1.00">
+                        <input type="number" class="form-control" id="berat" placeholder="1.00">
                     </div>
                 </div>
             </div>
@@ -94,7 +93,43 @@
         </div>
       </div>
     </div>
-  <!-- end card-body -->
+  </div>
+  <div class="card">
+    <div class="card-header align-items-center d-flex">
+      <h4>Tarif Ongkir</h4>
+    </div>
+    <div class="card-body">
+      <table class="table table-striped table-nowrap align-middle mb-0">
+        <thead>
+            <tr>
+                <th scope="col">Ekspedisi</th>
+                <th scope="col">Layanan</th>
+                <th scope="col">Estimasi Waktu</th>
+                <th scope="col">Biaya</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><img src="{{asset('assets/images/jne.png')}}" alt="" style="width: 25%;"></td>
+                <td>Bobby Davis</td>
+                <td>Nov 14, 2021</td>
+                <td>$2,410</td>
+            </tr>
+            <tr>
+                <td><img src="{{asset('assets/images/pos.png')}}" alt="" style="width: 25%;"></td>
+                <td>Christopher Neal</td>
+                <td>Nov 21, 2021</td>
+                <td>$1,450</td>
+            </tr>
+            <tr>
+                <td><img src="{{asset('assets/images/tiki.png')}}" alt="" style="width: 25%;"></td>
+                <td>Christopher Neal</td>
+                <td>Nov 21, 2021</td>
+                <td>$1,450</td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
   </div>
 @endsection
 
